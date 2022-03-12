0fd2d76699f24ade9665985ac4f92e64e198f105
oxeedo <101225414+oxeedo@users.noreply.github.com>
2022-03-12 23:57:59 +0100

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import{UserDataService} from './services/user-data.service';
+import{NgForm}from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'forms in angular';
+  
+  
+  
+}
+
+
+  
